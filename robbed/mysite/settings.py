3b673c1026350b5419bb65ca897e6940bda3aed6
Cristian Araya <caraya@alumnos.inf.utfsm.cl>
2017-12-19 12:01:14 -0300

    Arreglando el problema con los statics

--- robbed/mysite/settings.py
+++ robbed/mysite/settings.py
@@ -94,7 +94,6 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_S3_SECURE_URLS = False
 AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
@@ -103,4 +102,4 @@ STATICFILES_DIRS = [
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = '/static/'
+AWS_S3_SECURE_URLS = False
